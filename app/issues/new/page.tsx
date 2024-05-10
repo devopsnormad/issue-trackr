0d29756efdd2143d95e376d8d93b0f5d3ef032a8
devopsnormad <devopsnormad@gmail.com>
2024-05-10 16:31:42 +0100

    Extract Error message component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -6,53 +6,68 @@ import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import {useState} from 'react'
-import {zodResolver} from "@hookform/resolvers/zod"
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchema";
-import {z} from "zod"
+import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuepage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit, formState: {errors} } = useForm<IssueForm>({
-    resolver: zodResolver(createIssueSchema)
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
   });
 
-  const [error, setError]= useState('')
+  const [error, setError] = useState("");
 
   return (
     <div className="max-w-xl">
-      {error && <Callout.Root color="red" className="mb-3">
+      {error && (
+        <Callout.Root color="red" className="mb-3">
           <Callout.Text>{error}</Callout.Text>
-        </Callout.Root>}
-    <form
-      className="space-y-3"
-      onSubmit={handleSubmit(async (data) => {
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
           try {
             await axios.post("/api/issues", data);
             router.push("/issues");
-            
           } catch (error) {
-           setError ('An unexpected error occurred.');
+            setError("An unexpected error occurred.");
           }
-      })}
-    >
-      <TextField.Root
-        placeholder="Title"
-        {...register("title")}
-      ></TextField.Root>
-      {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
-      {errors.description && <Text color="red" as="p">{errors.description.message}</Text>}
-      <Button>Submit New Issue</Button>
-    </form>
+        })}
+      >
+        <TextField.Root
+          placeholder="Title"
+          {...register("title")}
+        ></TextField.Root>
+        
+          <ErrorMessage>
+            {errors.title?.message}
+          </ErrorMessage>
+       
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        
+          <ErrorMessage>
+            {errors.description?.message}
+          </ErrorMessage>
+        
+        <Button>Submit New Issue</Button>
+      </form>
     </div>
   );
 };
